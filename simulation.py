0572cbfc0e6725999e60a784f70acbee3bcc209f
jaebooker <jaesonbooker@gmail.com>
2021-09-25 15:50:00 +0100

    adding initial progress points

--- simulation.py
+++ simulation.py
@@ -4,11 +4,12 @@ from logger import *
 from agent import *
 class Simulation(object):
 
-    def __init__(self, _agent_size, _threshold, _initial_cooperation=1):
+    def __init__(self, _agent_size, _threshold, _diffierent_init_progress, _initial_cooperation=1):
         self.agent_size = _agent_size
         self.population = []
         self.next_agent_id = 0
         self.threshold = _threshold
+        self.diffierent_init_progress = _diffierent_init_progress
         self.file_name = "_simulation_pop_{}_th_{}_cooperate_{}.txt".format(
             _agent_size, _threshold, _initial_cooperation)
 
@@ -28,22 +29,26 @@ class Simulation(object):
         population = []
         cooperate_count = 0
         while len(population) != agent_size:
-            if cooperate_count !=  _initial_cooperation:
-                agent = Agent(self.next_agent_id, True)
-                population.append(agent)
-                cooperate_count += 1
-            else:
+            if self.diffierent_init_progress:
                 number = random.randrange(0,100)
-                if (number / 100) < self.threshold:
-                    agent = Agent(self.next_agent_id, False)
-                elif (number / 100) >= self.threshold:
+                if cooperate_count !=  _initial_cooperation:
+                    agent = Agent(self.next_agent_id, True, number / 100)
+                    population.append(agent)
+                    cooperate_count += 1
+                else:
+                    agent = Agent(self.next_agent_id, False, number / 100)
+            else:
+                if cooperate_count !=  _initial_cooperation:
                     agent = Agent(self.next_agent_id, True)
-                population.append(agent)
+                    population.append(agent)
+                    cooperate_count += 1
+                else:
+                    agent = Agent(self.next_agent_id, False)
             self.next_agent_id += 1
         return population
 
     def simulation_should_continue(self):
-        # 
+        # AI has not reached full maturity
         # 
         # 
         #     
@@ -88,9 +93,10 @@ if __name__ == "__main__":
     params = sys.argv[1:]
     agent_size = int(params[0])
     threshold = float(params[1])
-    if len(params) == 2:
-        initial_cooperation = int(params[2])
+    diffierent_init_progress = bool(params[2])
+    if len(params) == 3:
+        initial_cooperation = int(params[3])
     else:
         initial_cooperation = 1
-    simulation = Simulation(agent_size, threshold, initial_cooperation)
+    simulation = Simulation(agent_size, threshold, diffierent_init_progress, initial_cooperation)
     simulation.run()
