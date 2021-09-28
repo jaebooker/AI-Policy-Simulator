e65b3e97a208fc60ba29a722e57c51cad08e56e9
jaebooker <jaesonbooker@gmail.com>
2021-09-28 15:41:34 +0100

    added comments to logger class

--- logger.py
+++ logger.py
@@ -23,20 +23,21 @@ class Logger(object):
         self.file_name = file_name
 
     def write_metadata(self, _agent_size, _threshold, _different_init_progress, _initial_cooperation):
-        # 
-        # 
-        # 
+        # logs number of agents and relevant details
+
         with open(self.file_name, "w") as f:
             f.write("Agents: {}\t Threshold: {}\t Different Initial Progress: {}\t Initial Cooperation: {}\n".format(_agent_size, _threshold, _different_init_progress, _initial_cooperation))
         f.closed
     def log_interaction(self, _agent1, _agent2, _type, _did_cooperate_or_defect):
+        #logs if an agent defected or cooperated
+
         with open(self.file_name, "a") as f:
             f.write("Agent 1: {}\t Agent 2: {}\t {} : {} \n".format(_agent1._id, _agent2._id, _type, _did_cooperate_or_defect))
         f.closed
 
     def log_time_step(self, time_step_number):
-        # 
-        # 
+        # logs each iteration
+
         with open(self.file_name, "a") as f:
             f.write("Time Steps: {}\n".format(time_step_number))
         f.closed
