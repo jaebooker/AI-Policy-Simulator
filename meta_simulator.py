95536f260c2feb41c33aba232bcbc77256146d30
jaebooker <jaesonbooker@gmail.com>
2021-11-19 00:47:55 +0000

    meta sim

--- meta_simulator.py
+++ meta_simulator.py
@@ -5,7 +5,7 @@ class Meta_Simulator(object):
     '''
     '''
 
-    def __init__(self, _init_progress):
+    def __init__(self, _agent_size, _threshold, _different_init_progress, _initial_cooperation, _initial_spies, _initial_imposters):
         # 
         # 
         self.init_progress = _init_progress
