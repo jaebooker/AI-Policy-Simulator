b9dab241833573362b730b918c1808c1a0ba15bf
jaebooker <jaesonbooker@gmail.com>
2021-11-22 14:37:36 +0000

    cooperative

--- cooperative.py
+++ cooperative.py
@@ -1,5 +1,6 @@
 from simulation import *
 from logger import *
+from agent import *
 
 class Cooperative(object):
     '''
@@ -10,3 +11,11 @@ class Cooperative(object):
         # 
         self.progress = _init_progress
         self.members = []
+
+    def did_cooperate(self, agent):
+        self.members.append(agent)
+        self.progress += agent.progress
+
+    def did_defect(self, agent):
+        self.members.remove(agent)
+        self.progress -= agent.progress
